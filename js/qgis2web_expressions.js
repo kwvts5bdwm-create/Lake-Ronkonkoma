4aa75363cda963832e12ca91577ec63d4c8aa815
kwvts5bdwm-create <kwvts5bdwm@privaterelay.appleid.com>
2026-05-27 09:28:37 -0400

    Upload 6
    Door to Door only with size 15 bold labels

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -757,7 +757,7 @@ function fnc_project_color(values, context) {
 
 
 
-function exp_label_D2D_1_eval_expression(context) {
+function exp_label_D2DFilteredGeo_1_eval_expression(context) {
     // number + '-' + locality
 
     var feature = context.feature;
@@ -770,7 +770,7 @@ function exp_label_D2D_1_eval_expression(context) {
 }
 
 
-function exp_label_D2D_1_eval_expression(context) {
+function exp_label_D2DFilteredGeo_1_eval_expression(context) {
     // number + '-' + locality
 
     var feature = context.feature;
